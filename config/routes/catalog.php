348366643f1426edcf96cd0951c8499d58d85daa
gregor <gregor.zoonoid@gmail.com>
2015-10-28 23:01:43 +0300

    Catalog element route
    Signed-off-by: gregor <gregor.zoonoid@gmail.com>

--- config/routes/catalog.php
+++ config/routes/catalog.php
@@ -1,6 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array (
+	'catalog_element' => array(
+		'uri_callback' => '/<element_uri>.html(?<query>)',
+		'defaults' => array(
+			'directory' => 'modules',
+			'controller' => 'catalog',
+			'action' => 'detail',
+		)
+	),
 	'catalog' => array(
 		'uri_callback' => array('Helper_Catalog', 'route'), 
 		'regex' => '(/<category_uri>(/<element_uri>.html))(?<query>)',
